df0043d046f63fbfdf810ca095999a15c15248dc
Ravi Teja <opsera@opseras-MacBook-Pro.local>
2022-03-03 00:20:17 -0800

    OPL-2144 Added changes related to lib

--- src/main/java/com/opsera/ansible/AnsibleServiceApplication.java
+++ src/main/java/com/opsera/ansible/AnsibleServiceApplication.java
@@ -2,18 +2,17 @@ package com.opsera.ansible;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author sreeni
  *
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.opsera.core", "com.opsera.ansible"})
 public class AnsibleServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnsibleServiceApplication.class, args);
     }
-    
-    
-
 }
