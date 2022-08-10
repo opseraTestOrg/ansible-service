82a7cc73012f1a28a4b09f66ed39431f9106f40f
Purusothaman Srinivasan <60213224+purusothamanns@users.noreply.github.com>
2022-08-10 08:09:03 +0530

    Vulnarbilites fix

--- src/main/java/com/opsera/ansible/config/ServiceFactory.java
+++ src/main/java/com/opsera/ansible/config/ServiceFactory.java
@@ -3,32 +3,23 @@
  */
 package com.opsera.ansible.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
 import com.opsera.ansible.service.CommandService;
 import com.opsera.ansible.service.VaultService;
 
-import lombok.Getter;
-import lombok.experimental.Accessors;
-
 /**
  * @author sreeni
  *
  */
-@Getter
 @Component
-public class ServiceFactory {
+public interface ServiceFactory {
 
-    @Autowired
-    @Accessors(fluent = true)
-    private Gson gson;
+    public Gson gson();
 
-    @Autowired
-    private CommandService commandService;
+    public CommandService getCommandService();
 
-    @Autowired
-    private VaultService vaultHelper;
+    public VaultService getVaultHelper();
 
 }
