f4611dc377490fff9d3a89b8ddbbbd4f2a67eb3a
Purusothaman Srinivasan <60213224+purusothamanns@users.noreply.github.com>
2022-08-10 18:28:29 +0530

    dependency issue fix

--- src/test/java/com/opsera/ansible/AnsibleServiceApplicationTests.java
+++ src/test/java/com/opsera/ansible/AnsibleServiceApplicationTests.java
@@ -1,13 +1,13 @@
 package com.opsera.ansible;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AnsibleServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
